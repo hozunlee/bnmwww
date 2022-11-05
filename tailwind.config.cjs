04959309c4cf8f68d9039c09bc983589161dd34f
ho2yahh <ho2yahh@gmail.com>
2022-11-05 11:24:39 +0900

    init

---
+++ tailwind.config.cjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'one-red': '#8AAAE5',
+        'two-yellow': '#FEFEFE',
+      },
+    },
+    screens: {
+      '3xl': { max: '2000px' },
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // md 이상 큰거
+      // md 미만 모바일
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+    },
+  },
+  plugins: [],
+};
