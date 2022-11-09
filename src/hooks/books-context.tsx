f7cbbdd8391e7b2c8df31ac06451ac0757083a6c
ho2yahh <ho2yahh@gmail.com>
2022-11-10 01:33:26 +0900

    feat : addMark 함수 구현

--- src/hooks/books-context.tsx
+++ src/hooks/books-context.tsx
@@ -7,16 +7,46 @@ const SampleData = {
     {
       id: 1,
       title: 'My Private Book',
+      marks: [
+        {
+          id: 1,
+          title: 'Title',
+          image:
+            'https://tailwindcss.com/_next/static/media/social-card-large.a6e71726.jpg',
+          description: 'ddd',
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'My Private Book',
       marks: [],
     },
   ],
 };
 
+const iem = {
+  id: 2,
+  title: '나는야 ',
+  image:
+    'https://tailwindcss.com/_next/static/media/social-card-large.a6e71726.jpg',
+  description: '코딩남',
+};
+
 export const BooksProvider = ({ children }) => {
-  const [books, setBooks] = useState(SampleData);
+  const [library, setLibrary] = useState(SampleData.books);
+
+  const addMarkItem = (id) => {
+    const findIndex = library.findIndex((item) => item.id === id);
+    const temp = [...library];
+    temp[findIndex].marks.push(iem);
+    setLibrary(temp);
+  };
 
   return (
-    <BooksContext.Provider value={{ books }}>{children}</BooksContext.Provider>
+    <BooksContext.Provider value={{ library, addMarkItem }}>
+      {children}
+    </BooksContext.Provider>
   );
 };
 
